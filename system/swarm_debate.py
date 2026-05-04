ba84307629ffa8a617cda4c693c62ac4f9256bea
wests-cmd <westd613@gmail.com>
2026-05-04 07:42:19 -0700

    Create swarm_debate.py

---
+++ system/swarm_debate.py
@@ -0,0 +1,11 @@
+import random
+
+class SwarmDebate:
+    def generate(self, task, swarm):
+        return [
+            {"knight": k["name"], "score": random.random()}
+            for k in swarm
+        ]
+
+    def select(self, plans):
+        return sorted(plans, key=lambda x: x["score"], reverse=True)[0]
