b3caaa32cee4e4546be9e190b9b911979df45fc4
wests-cmd <westd613@gmail.com>
2026-05-04 08:11:48 -0700

    Update toolscan.py

--- modules/toolscan.py
+++ modules/toolscan.py
@@ -1,10 +1,5 @@
 import shutil
 
-class ToolScan:
-    def scan(self):
-        tools = {}
-        for tool in ["python3", "docker", "git", "ffmpeg", "nmap"]:
-            path = shutil.which(tool)
-            if path:
-                tools[tool] = {"path": path, "known": True}
-        return tools
+def scan():
+    tools = ["python", "git", "docker"]
+    return {t: bool(shutil.which(t)) for t in tools}
