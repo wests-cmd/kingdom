331e4ddcdf6a6acffa3566ade9e6691d0dd8feaa
wests-cmd <westd613@gmail.com>
2026-05-04 09:43:52 -0700

    Create task_queue.py

---
+++ system/task_queue.py
@@ -0,0 +1,9 @@
+class TaskQueue:
+    def __init__(self):
+        self.queue = []
+
+    def add(self, task):
+        self.queue.append(task)
+
+    def get(self):
+        return self.queue.pop(0) if self.queue else None
