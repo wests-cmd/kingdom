9987e580b1b3fbb448d3c5e7834948ad2b640da1
wests-cmd <westd613@gmail.com>
2026-05-07 12:01:51 -0700

    Delete system directory

--- system/task_queue.py
+++
@@ -1,9 +0,0 @@
-class TaskQueue:
-    def __init__(self):
-        self.queue = []
-
-    def add(self, task):
-        self.queue.append(task)
-
-    def get(self):
-        return self.queue.pop(0) if self.queue else None
