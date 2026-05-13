1255c171d3781842713e8a07da689751121318e0
wests-cmd <westd613@gmail.com>
2026-05-13 16:04:22 -0700

    Create AIMap.jsx

---
+++ frontend/src/pages/AIMap.jsx
@@ -0,0 +1,11 @@
+import AIMapGraph from "../components/graphs/AIMapGraph"
+
+export default function AIMap() {
+
+  return (
+    <div>
+      <h2>AI Map</h2>
+      <AIMapGraph />
+    </div>
+  )
+}
