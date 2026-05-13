e2db50e6fe73e18649eef25db834d589b7190e61
wests-cmd <westd613@gmail.com>
2026-05-13 16:05:12 -0700

    Create Runtime.jsx

---
+++ frontend/src/pages/Runtime.jsx
@@ -0,0 +1,11 @@
+import RuntimeControls from "../components/runtime/RuntimeControls"
+
+export default function Runtime() {
+
+  return (
+    <div>
+      <h2>Runtime</h2>
+      <RuntimeControls />
+    </div>
+  )
+}
