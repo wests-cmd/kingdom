cb7197163291d926e3e107ea7e3b88a7c5550d4b
wests-cmd <westd613@gmail.com>
2026-05-13 16:04:53 -0700

    Create Memory.jsx

---
+++ frontend/src/pages/Memory.jsx
@@ -0,0 +1,11 @@
+import MemoryGraph from "../components/graphs/MemoryGraph"
+
+export default function Memory() {
+
+  return (
+    <div>
+      <h2>Memory Graph</h2>
+      <MemoryGraph />
+    </div>
+  )
+}
