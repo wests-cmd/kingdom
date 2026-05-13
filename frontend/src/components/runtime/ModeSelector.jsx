fb6ae9e178e5b32d690eaec8e7320781e5c318b4
wests-cmd <westd613@gmail.com>
2026-05-13 16:10:44 -0700

    Create ModeSelector.jsx

---
+++ frontend/src/components/runtime/ModeSelector.jsx
@@ -0,0 +1,13 @@
+export default function ModeSelector() {
+
+  return (
+    <select>
+
+      <option>persistent</option>
+      <option>adaptive</option>
+      <option>burst</option>
+      <option>scheduled</option>
+
+    </select>
+  )
+}
