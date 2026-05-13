a6e212c9acbaad35d0267b725d1249112f33ef1d
wests-cmd <westd613@gmail.com>
2026-05-13 16:01:36 -0700

    Create Swarm.jsx

---
+++ frontend/src/Swarm.jsx
@@ -0,0 +1,11 @@
+import SwarmGraph from "../components/graphs/SwarmGraph"
+
+export default function Swarm() {
+
+  return (
+    <div>
+      <h2>Swarm</h2>
+      <SwarmGraph />
+    </div>
+  )
+}
