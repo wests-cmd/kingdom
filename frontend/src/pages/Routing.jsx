c443fada5cc72675be5b6df172b3fb8e1ba08cec
wests-cmd <westd613@gmail.com>
2026-05-13 16:05:42 -0700

    Create Routing.jsx

---
+++ frontend/src/pages/Routing.jsx
@@ -0,0 +1,11 @@
+import RoutingGraph from "../components/graphs/RoutingGraph"
+
+export default function Routing() {
+
+  return (
+    <div>
+      <h2>Routing</h2>
+      <RoutingGraph />
+    </div>
+  )
+}
